7df64ae66c6fc7e4770a23dc167213389f819442
MonzerOmer1234 <monzeromer048@gmail.com>
2024-05-23 14:30:29 +0200

    remove update

--- src/components/profile-finder/ProfileFinder.jsx
+++ src/components/profile-finder/ProfileFinder.jsx
@@ -42,7 +42,7 @@ export default function ProfileFinder() {
         <input
           type="text"
           value={username}
-          onChange={(event) => setUsername(event.target.value.toLowerCase())}
+          onChange={(event) => setUsername(event.target.value)}
           name="search-by-username"
           placeholder="Search Github Username..."
         />
